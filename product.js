77d354d142f8d3c09343a45ccac4544854d49641
the-off-store <rowdysingham92@gmail.com>
2026-03-29 16:48:52 +0530

    Create product.js

---
+++ product.js
@@ -0,0 +1,37 @@
+const inventory = [
+    {
+        category: "KIT", // Emergency Kit
+        name: "10-in-1 Emergency Puncture Kit",
+        price: 599,
+        searchKeywords: "puncture kit tyre repair tool",
+        // Add 3+ images here from any website
+        images: [
+            "https://m.media-amazon.com/images/I/71z1S4K8jLL._SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/71fB8G-pEIL._SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/61lYJzUuYPL._SL1500_.jpg"
+        ]
+    },
+    {
+        category: "LIT", // Lighting
+        name: "Rechargeable LED Tactical Flashlight",
+        price: 349,
+        searchKeywords: "torch light led flashlight zoomable",
+        images: [
+            "https://m.media-amazon.com/images/I/61H4qG6m7rL._SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/61X-Hw8MhFL._SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/61nSj3LdYpL._SL1500_.jpg"
+        ]
+    },
+    {
+        category: "BIK", // Bike Gear
+        name: "Waterproof Mobile Holder for Bikes",
+        price: 450,
+        searchKeywords: "mobile holder bike waterproof gps mount",
+        images: [
+            "https://m.media-amazon.com/images/I/61S-lX7rK6L._SL1100_.jpg",
+            "https://m.media-amazon.com/images/I/61jD7n+R-EL._SL1100_.jpg",
+            "https://m.media-amazon.com/images/I/71Y9tV1VbXL._SL1100_.jpg"
+        ]
+    }
+    // YOU CAN COPY-PASTE MORE ITEMS HERE LATER
+];
